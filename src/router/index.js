e18be31119e82fc4db772fbc919d9b24803a9d7c
MAROUA <144175713+marwamajdoub@users.noreply.github.com>
2024-07-03 13:28:14 +0100

    update
    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../components/HomePage.vue';
 import FormBuilder from '../components/FormBuilder.vue';
-import LoginPage from '../components/LoginPage.vue'; // Mettez à jour le chemin vers LoginPage.vue
+import LoginPage from '../components/LoginPage.vue';
+import AdminPage from '../components/AdminPage.vue'; // Chemin vers AdminPage correct
+import SignUp from '../components/SignUp.vue'; // Chemin vers SignUp correct
+import firebase from '../firebaseConfig'; // Chemin vers firebaseConfig.js correct
 
 const routes = [
   {
@@ -11,7 +14,7 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: LoginPage // Utilisez le composant LoginPage
+    component: LoginPage
   },
   {
     path: '/home',
@@ -22,12 +25,54 @@ const routes = [
     path: '/form-builder',
     name: 'FormBuilder',
     component: FormBuilder
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: AdminPage,
+    meta: { requiresAdmin: true } // Protection d'accès admin
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+// Garde de navigation pour les routes nécessitant un rôle d'administration
+router.beforeEach((to, from, next) => {
+  const requiresAdmin = to.matched.some(record => record.meta.requiresAdmin);
+  if (requiresAdmin) {
+    const user = firebase.auth().currentUser;
+    if (user) {
+      firebase.firestore().collection('users').doc(user.uid).get()
+        .then((doc) => {
+          if (doc.exists) {
+            const userData = doc.data();
+            if (userData.role === 'admin') {
+              next();
+            } else {
+              next('/home'); // Rediriger vers la page d'accueil si non admin
+            }
+          } else {
+            console.error('Aucun document utilisateur trouvé');
+            next('/login');
+          }
+        }).catch((error) => {
+          console.error('Erreur lors de la récupération du document utilisateur:', error);
+          next('/login');
+        });
+    } else {
+      next('/login');
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
